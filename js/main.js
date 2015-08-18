5a84f147dd0ec6a74bc02856cbb96fce10902eba
Dinh Tran <mrtrandinhvn@gmail.com>
2015-08-19 01:51:44 +0700

    Version 1.0: 1st stable version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Used for PayrollManager pages
 var doLoginPayrollManager = function (userObj) {
     var username = userObj.username;
     var password = userObj.password;
@@ -10,15 +11,57 @@ var doLoginPayrollManager = function (userObj) {
     (document.head || document.documentElement).appendChild(script);
     script.parentNode.removeChild(script);
 }
-var doNormalLogin = function () {
-    // inject script into the page after the page has loaded
-    var s = document.createElement('script');
-    // TODO: add "script.js" to web_accessible_resources in manifest.json
-    s.src = chrome.extension.getURL('js/donormallogin.js');
-    s.onload = function () {
-        this.parentNode.removeChild(this);
-    };
-    (document.head || document.documentElement).appendChild(s);
+// Select User Role
+var doLoginWithRole = function () {
+    var insertedNodes = [];
+    var observer = new MutationObserver(function (mutations) {
+        var roleList = $('select option');
+        var isTriggered = false;
+        for (var i = 0; i < mutations.length; i++) {
+            if (mutations[i].attributeName == "style") {
+                isTriggered = true;
+            }
+        }
+        if (isTriggered) {
+            if (roleList.length > 2) {
+                for (var i = 0; i < roleList.length; i++) {
+                    if ($(roleList[i]).text() == "Manager") {
+                        $(roleList[i]).prop('selected', true);
+                        var script = document.createElement('script');
+                        script.textContent = "$('select').change();";
+                        (document.head || document.documentElement).appendChild(script);
+                        script.parentNode.removeChild(script);
+                        break;
+                    }
+                }
+            }
+        }
+    });
+    var roleSelectNode = $('.brID-RoleType')[0];
+    observer.observe(roleSelectNode, { attributes: true });
+
+}
+// Specific action to do login in normal honey pages
+var doLoginActions = function (userObj) {
+    $('.brID-Username input').val(userObj.username);
+    $('.brID-Username input').focus();
+    $('.brID-Password input').val(userObj.password);
+    $('.brID-Password input').focus();
+    $('.btn_login .brID-actionButton.br-button input').focus();
+    $('.btn_login .brID-actionButton.br-button input').click();
+    doLoginWithRole();
+}
+
+// Used for all normal honey pages
+var doNormalLogin = function (userObj) {
+    if (!document.hasFocus()) {
+        // can't get focus priority from console so I have to set this event.
+        $(window).focus(function () {
+            doLoginActions(userObj);
+        });
+    } else {
+        doLoginActions(userObj)
+    }
 }
 var setSpecialLoginData = function (username, password) {
     var userObj = {
@@ -46,6 +89,18 @@ $(document).ready(function () {
         }
         doLoginPayrollManager(userObj);
     } else if (normalPattern.test(currentURL)) {
-        doNormalLogin();
+        var userObj = {
+            username: "admin",
+            password: "12345"
+        }
+        switch (true) {
+            case /draketest.honeysoftware.co.nz/.test(currentURL):
+                userObj = setSpecialLoginData("admin", "aaa");
+                break;
+            case /hiltontest-adminnoncrm/.test(currentURL):
+                userObj = setSpecialLoginData("phil", "123");
+                break;
+        }
+        doNormalLogin(userObj);
     }
 });
